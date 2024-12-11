9e61a7645fe881c5e3d042b76685a4b433113f02
yunzhewong <84904965+yunzhewong@users.noreply.github.com>
2024-12-12 09:06:49 +1100

    day 2 in python

--- 2/2.py
+++ 2/2.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from typing import List
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
@@ -10,24 +11,67 @@ def same_sign(number1, number2):
     return number1 * number2 >= 0
 
 
+def valid_range(diff):
+    return abs(diff) <= 3 and abs(diff) >= 1
+
+
+def get_numbers(line: str):
+    return [int(i) for i in line.split(" ")]
+
+
+def is_safe(numbers):
+    first_diff = numbers[1] - numbers[0]
+
+    for i in range(len(numbers) - 1):
+        diff = numbers[i + 1] - numbers[i]
+
+        if not same_sign(first_diff, diff):
+            return False
+
+        if not valid_range(diff):
+            return False
+
+    return True
+
+
 def two_a():
     total_safe = [0]
 
     def fn(stripped_line: str):
-        numbers = [int(i) for i in stripped_line.split(" ")]
+        numbers = get_numbers(stripped_line)
 
-        first_diff = numbers[1] - numbers[0]
+        if is_safe(numbers):
+            total_safe[0] += 1
 
-        for i in range(len(numbers) - 1):
-            diff = numbers[i + 1] - numbers[i]
+    operate_on_lines(fn)
 
-            if not same_sign(first_diff, diff):
+    print(total_safe[0])
+
+
+def identify_dominant_number_sign(numbers: List[int]):
+    if len(numbers) < 4:
+        return None
+    start_diffs = [numbers[i + 1] - numbers[i] for i in range(3)]
+    start_diffs.sort()
+    return start_diffs[1]
+
+
+def two_b():
+    total_safe = [0]
+
+    def fn(stripped_line: str):
+        numbers = get_numbers(stripped_line)
+
+        if is_safe(numbers):
+            total_safe[0] += 1
+            return
+
+        for i in range(len(numbers)):
+            new_numbers = numbers[:i] + numbers[i + 1 :]
+            safe = is_safe(new_numbers)
+            if safe:
+                total_safe[0] += 1
                 return
-
-            if abs(diff) > 3 or abs(diff) < 1:
-                return
-
-        total_safe[0] += 1
 
     operate_on_lines(fn)
 
@@ -35,4 +79,4 @@ def two_a():
 
 
 if __name__ == "__main__":
-    two_a()
+    two_b()
