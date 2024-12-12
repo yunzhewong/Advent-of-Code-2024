d7d113a54dea207edb8b2d3018e05a2710989049
yunzhewong <84904965+yunzhewong@users.noreply.github.com>
2024-12-13 10:13:55 +1100

    implemented a map function

--- 5/5.cpp
+++ 5/5.cpp
@@ -1,5 +1,6 @@
 #include "5.h"
 
+#include <algorithm>
 #include <iostream>
 #include <sstream>
 #include <unordered_map>
@@ -13,26 +14,63 @@ struct Rule {
   int y;
 };
 
-Rule parse_rule(std::string line) {
-  std::vector<std::string> strings = utils::split(line, ' ');
-  std::vector<int> numbers;
+int toInt(std::string val) { return std::stoi(val); }
 
-  for (std::string s : strings) {
-    numbers.push_back(std::stoi(s));
-  }
+Rule parse_rule(std::string line) {
+  std::vector<std::string> strings = utils::split(line, '|');
+  std::vector<int> numbers = utils::map<std::string, int>(strings, toInt);
 
   return Rule{numbers[0], numbers[1]};
 }
 
-void five_a() {
-  int total = 0;
+std::vector<int> parse_updates(std::string line) {}
+
+struct FileInfo {
   std::unordered_map<int, std::vector<int>> rules;
+  std::vector<std::vector<int>> all_updates;
+};
+
+FileInfo parse_file() {
+  bool found_space = false;
+  FileInfo info;
 
   operate_on_lines(5, [&](std::string line) {
-    std::cout << line << "\n";
-    total += 1;
+    if (line == "") {
+      found_space = true;
+      return;
+    }
+
+    if (!found_space) {
+      Rule rule = parse_rule(line);
+      info.rules[rule.x].push_back(rule.y);
+      return;
+    }
   });
   std::cout << total << "\n";
 }
+// state = State()
+
+//     all_updates = []
+
+//                   def
+//                   fn(line
+//                      : str)
+//     : if line == "" : state.found_space = True return
+
+//                                           if not state.found_space : x,
+// y = parse_rule(line)
+
+//     arr = state.rules.get(x, []) arr.append(y) state.rules[x] = arr return
+
+//     updates = parse_updates(line) all_updates
+//                   .append(updates)
+
+//                       operate_on_lines(fn)
+
+//                           return state.rules,
+// all_updates
+}
+
+void five_a() {}
 
 void five() { five_a(); };
--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,18 @@
+#include <functional>
 #include <string>
 #include <vector>
 
 namespace utils {
 std::vector<std::string> split(std::string& string, char character);
-}
+
+template <typename K, typename V>
+std::vector<V> map(std::vector<K> original, std::function<V(K)> fn) {
+  std::vector<V> output;
+
+  for (K val : original) {
+    output.push_back(fn(val));
+  }
+
+  return output;
+};
+}  // namespace utils
