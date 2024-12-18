b06d620724ceea8f81df53654c02ea354a572fab
yunzhewong <84904965+yunzhewong@users.noreply.github.com>
2024-12-18 12:22:32 +1100

    8b done

--- 8/8.py
+++ 8/8.py
@@ -42,22 +42,44 @@ def in_board(size, position):
     )
 
 
-def evaluate_antinodes(antennas):
+def evaluate_antinodes(antennas, stopFn):
     antinodes = []
 
     for i in range(len(antennas)):
         for j in range(i + 1, len(antennas)):
-            antinodes += get_antinodes(antennas[i], antennas[j])
+            antinodes += get_antinodes(antennas[i], antennas[j], stopFn)
 
     return antinodes
 
 
-def get_antinodes(antenna1, antenna2):
+def get_antinodes(antenna1, antenna2, stopFn):
     oneToTwo = (antenna2[0] - antenna1[0], antenna2[1] - antenna1[1])
-    anti1 = (antenna1[0] - oneToTwo[0], antenna1[1] - oneToTwo[1])
-    anti2 = (antenna2[0] + oneToTwo[0], antenna2[1] + oneToTwo[1])
 
-    return [anti1, anti2]
+    antinodes = []
+
+    backward_dir = 1
+    while True:
+        direction_one = (
+            antenna1[0] - backward_dir * oneToTwo[0],
+            antenna1[1] - backward_dir * oneToTwo[1],
+        )
+        antinodes.append(direction_one)
+        if stopFn(direction_one, backward_dir):
+            break
+        backward_dir += 1
+
+    forward_dir = 0
+    while True:
+        direction_two = (
+            antenna1[0] + forward_dir * oneToTwo[0],
+            antenna1[1] + forward_dir * oneToTwo[1],
+        )
+        antinodes.append(direction_two)
+        if stopFn(direction_two, forward_dir):
+            break
+        forward_dir += 1
+
+    return antinodes
 
 
 def eight_a():
@@ -65,8 +87,33 @@ def eight_a():
 
     all_antinodes = []
 
+    def stopFn(_, count):
+        return count >= 1
+
     for frequency in nodes.keys():
-        antinodes = evaluate_antinodes(nodes[frequency])
+        antinodes = evaluate_antinodes(nodes[frequency], stopFn)
+        for antinode in antinodes:
+            if not in_board(size, antinode):
+                continue
+
+            if antinode in all_antinodes:
+                continue
+
+            all_antinodes.append(antinode)
+
+    print(len(all_antinodes))
+
+
+def eight_b():
+    size, nodes = parse_file()
+
+    all_antinodes = []
+
+    def stopFn(pos, _):
+        return not in_board(size, pos)
+
+    for frequency in nodes.keys():
+        antinodes = evaluate_antinodes(nodes[frequency], stopFn)
         for antinode in antinodes:
             if not in_board(size, antinode):
                 continue
@@ -80,4 +127,4 @@ def eight_a():
 
 
 if __name__ == "__main__":
-    eight_a()
+    eight_b()
