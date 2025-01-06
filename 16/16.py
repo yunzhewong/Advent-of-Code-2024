a915d7cdafb8114d6aae161e3f1ba70777f1c80b
yunzhewong <84904965+yunzhewong@users.noreply.github.com>
2025-01-06 22:12:46 +1100

    16a

--- 16/16.py
+++ 16/16.py
@@ -9,121 +9,81 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from utils.operate_on_lines import operate_on_lines, operate_on_test
 
-EPSILON = 0.01
 
+def read_maze():
+    maze = []
 
-def close_to_int(l: float):
-    return abs(l - round(l)) < EPSILON
-
-
-def parse_button_line(l: str):
-    first_plus_index = l.find("+")
-    first_comma_index = l.find(",")
-    x = int(l[first_plus_index + 1 : first_comma_index])
-
-    second_plus_index = l.find("+", first_comma_index + 1)
-    y = int(l[second_plus_index + 1 :])
-    return (x, y)
-
-
-def parse_prize_line(l: str):
-    first_equal_index = l.find("=")
-    first_comma_index = l.find(",")
-    x = int(l[first_equal_index + 1 : first_comma_index])
-
-    second_equal_index = l.find("=", first_equal_index + 1)
-    y = int(l[second_equal_index + 1 :])
-    return (x, y)
-
-
-def parse_input():
-    games = []
-
-    abpi = [None, None, None, 0]
-
-    def fn(l: str):
-        if abpi[3] % 4 == 0:
-            abpi[0] = parse_button_line(l)
-        if abpi[3] % 4 == 1:
-            abpi[1] = parse_button_line(l)
-        if abpi[3] % 4 == 2:
-            abpi[2] = parse_prize_line(l)
-        if abpi[3] % 4 == 3:
-            games.append((abpi[0], abpi[1], abpi[2]))
-        abpi[3] += 1
+    def fn(l):
+        maze.append(list(l))
 
     operate_on_lines(fn)
-    games.append((abpi[0], abpi[1], abpi[2]))
 
-    return games
-
-
-def dot(v1, v2):
-    return v1[0] * v2[0] + v1[1] * v2[1]
+    return maze
 
 
-def get_game_solution(game):
-    # [x_A; y_A] * A + [x_B; y_B] * B = [x_P; y_P]
-    # [x_A x_B; y_A y_B] * [A; B] = [x_P; y_P]
-    x_A, y_A = game[0]
-    x_B, y_B = game[1]
-    x_P, y_P = game[2]
-
-    idet = 1 / (x_A * y_B - y_A * x_B)
-
-    A = idet * (y_B * x_P - x_B * y_P)
-    B = idet * (-y_A * x_P + x_A * y_P)
-    if close_to_int(A) and close_to_int(B):
-
-        return round(A), round(B)
-
-    return None
+def move(position, direction):
+    return (position[0] + direction[0], position[1] + direction[1])
 
 
-def evaluate_solution_cost(solution):
-    return solution[0] * 3 + solution[1]
+def rotate_counterclockwise(direction):
+    return (direction[1], -direction[0])
 
 
-def thirteen_a():
-    games = parse_input()
-    total = 0
+def rotate_clockwise(direction):
+    return (-direction[1], direction[0])
 
-    for game in games:
-        solution = get_game_solution(game)
-        # print(solution)
-        if solution is None:
+
+def find_start(maze):
+    for i in range(len(maze)):
+        for j in range(len(maze[i])):
+            if maze[i][j] == "S":
+                return (i, j)
+    raise Exception("Start Expected")
+
+
+def is_wall(maze, position):
+    return maze[position[0]][position[1]] == "#"
+
+
+def sixteen_a():
+    maze = read_maze()
+
+    def sortkey(obj):
+        return obj[0]
+
+    start_pos = find_start(maze)
+
+    visited = []
+    queue = [(0, start_pos, (0, 1))]
+    while True:
+        if len(queue) == 0:
+            break
+
+        (score, position, direction) = queue.pop(0)
+        if position in visited:
             continue
-        cost = evaluate_solution_cost(solution)
-        total += cost
+        visited.append(position)
 
-    print(total)
+        if maze[position[0]][position[1]] == "E":
+            print(score, position, direction)
+            break
 
+        next_pos = move(position, direction)
+        if not is_wall(maze, next_pos):
+            queue.append((score + 1, next_pos, direction))
 
-MODIFICATION = 10000000000000
+        clockwise_dir = rotate_clockwise(direction)
+        clockwise_pos = move(position, clockwise_dir)
+        if not is_wall(maze, clockwise_pos):
+            queue.append((score + 1001, clockwise_pos, clockwise_dir))
 
+        counterclockwise_dir = rotate_counterclockwise(direction)
+        counterclockwise_pos = move(position, counterclockwise_dir)
+        if not is_wall(maze, counterclockwise_pos):
+            queue.append((score + 1001, counterclockwise_pos, counterclockwise_dir))
 
-def modify_game(game):
-    x_before, y_before = game[2]
-    x_after = x_before + MODIFICATION
-    y_after = y_before + MODIFICATION
-    return game[0], game[1], (x_after, y_after)
-
-
-def thirteen_b():
-    games = parse_input()
-    total = 0
-
-    for game in games:
-        modified_game = modify_game(game)
-        solution = get_game_solution(modified_game)
-        # print(solution)
-        if solution is None:
-            continue
-        cost = evaluate_solution_cost(solution)
-        total += cost
-
-    print(total)
+        queue.sort(key=sortkey)
 
 
 if __name__ == "__main__":
-    thirteen_b()
+    sixteen_a()
