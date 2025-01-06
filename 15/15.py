1101b4dd335e5fff5e2d22828a759ba3ff4b3425
yunzhewong <84904965+yunzhewong@users.noreply.github.com>
2025-01-06 19:32:08 +1100

    day 15 done

--- 15/15.py
+++ 15/15.py
@@ -3,7 +3,7 @@ import os
 import sys
 import time
 from enum import Enum
-from typing import Callable, Dict, List, Tuple
+from typing import Any, Callable, Dict, List, Tuple
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
@@ -57,6 +57,15 @@ def move_from_pos(pos, move):
     return (pos[0] + move[0], pos[1] + move[1])
 
 
+def shift_robot(robot_pos, movement, map):
+    map[robot_pos[0]][robot_pos[1]] = "."
+
+    next_robot_pos = move_from_pos(robot_pos, movement)
+    map[next_robot_pos[0]][next_robot_pos[1]] = "@"
+
+    return next_robot_pos
+
+
 def move_robot(robot_pos, move, map):
     next_pos = move_from_pos(robot_pos, move)
 
@@ -77,11 +86,7 @@ def move_robot(robot_pos, move, map):
         new_position = move_from_pos(position, move)
         map[new_position[0]][new_position[1]] = "O"
 
-    map[robot_pos[0]][robot_pos[1]] = "."
-
-    next_robot_pos = move_from_pos(robot_pos, move)
-    map[next_robot_pos[0]][next_robot_pos[1]] = "@"
-
+    next_robot_pos = shift_robot(robot_pos, move, map)
     return next_robot_pos
 
 
@@ -119,5 +124,114 @@ def fifteen_a():
     print(sum_of_box_positions(map))
 
 
+def expand_map(map):
+    expanded = []
+    for line in map:
+        expanded_line = []
+        for char in line:
+            if char == "#":
+                expanded_line.append("#")
+                expanded_line.append("#")
+            if char == "O":
+                expanded_line.append("[")
+                expanded_line.append("]")
+            if char == ".":
+                expanded_line.append(".")
+                expanded_line.append(".")
+            if char == "@":
+                expanded_line.append("@")
+                expanded_line.append(".")
+        expanded.append(expanded_line)
+    return expanded
+
+
+def get_box_positions(map, tip_pos):
+    tip_char = map[tip_pos[0]][tip_pos[1]]
+
+    if tip_char == "]":
+        return [move_from_pos(tip_pos, (0, -1)), tip_pos]
+    return [tip_pos, move_from_pos(tip_pos, (0, 1))]
+
+
+def order_boxes(box_positions: List[Any], movement):
+    if movement[1] == -1:
+        return box_positions[::-1]
+    return box_positions
+
+
+def get_next_tips(box_positions, movement):
+    if movement[0] != 0:
+        return [move_from_pos(pos, movement) for pos in box_positions]
+
+    if movement[1] == 1:
+        return [move_from_pos(box_positions[1], movement)]
+    return [move_from_pos(box_positions[0], movement)]
+
+
+def move_robot_b(robot_pos, movement, map):
+    next_pos = move_from_pos(robot_pos, movement)
+
+    boxes_to_move = []
+    search_tip_positions = [next_pos]
+
+    while len(search_tip_positions) > 0:
+        next_tips = []
+        for tip_pos in search_tip_positions:
+            tip_char = map[tip_pos[0]][tip_pos[1]]
+            if tip_char == "#":
+                return robot_pos
+
+            if tip_char == ".":
+                continue
+
+            box_positions = get_box_positions(map, tip_pos)
+            next_tips += get_next_tips(box_positions, movement)
+            boxes_to_move += order_boxes(box_positions, movement)
+        search_tip_positions = next_tips
+
+    moved_boxes = []
+
+    for i in range(len(boxes_to_move)):
+        old_position = boxes_to_move[len(boxes_to_move) - i - 1]
+        if old_position in moved_boxes:
+            continue
+        char = map[old_position[0]][old_position[1]]
+        map[old_position[0]][old_position[1]] = "."
+        new_position = move_from_pos(old_position, movement)
+        map[new_position[0]][new_position[1]] = char
+        moved_boxes.append(old_position)
+
+    next_robot_pos = shift_robot(robot_pos, movement, map)
+    return next_robot_pos
+
+
+def sum_box_coords_b(map):
+
+    total = 0
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            if map[i][j] == "[":
+                total += 100 * i + j
+    return total
+
+
+def fifteen_b():
+    map, movements = read_input()
+    map = expand_map(map)
+    pretty_print(map)
+
+    robot_pos = find_robot_position(map)
+
+    if robot_pos is None:
+        raise Exception("Not Possible")
+
+    for char_move in movements:
+        movement = read_move(char_move)
+        robot_pos = move_robot_b(robot_pos, movement, map)
+
+    pretty_print(map)
+    print(sum_box_coords_b(map))
+
+
 if __name__ == "__main__":
-    fifteen_a()
+    fifteen_b()
